4ae40c512f6aab2173ffdcb1be19f62abcfbb515
avril <u20231d637@upc.edu.pe>
2026-03-16 22:10:48 -0500

    feat(portfolio): add portfolio store for state management.

---
+++ src/portfolio/application/portfolio.store.js
@@ -0,0 +1,29 @@
+import { reactive } from 'vue'
+import { PortfolioData } from '../infrastructure/portfolio.data.js'
+import { DeveloperAssembler } from '../infrastructure/developer.assembler.js'
+import { ProjectAssembler } from '../infrastructure/project.assembler.js'
+import { SkillCategoryAssembler } from '../infrastructure/skill-category.assembler.js'
+
+const portfolioData = new PortfolioData()
+
+export const portfolioStore = reactive({
+    developer: null,
+    projects: [],
+    skillCategories: [],
+    currentProjectIndex: 0,
+
+    loadData() {
+        this.developer = DeveloperAssembler
+            .toEntityFromResource(portfolioData.getDeveloper())
+
+        this.projects = ProjectAssembler
+            .toEntitiesFromResource(portfolioData.getProjects())
+
+        this.skillCategories = SkillCategoryAssembler
+            .toEntitiesFromResource(portfolioData.getSkillCategories())
+    },
+
+    setCurrentProject(index) {
+        this.currentProjectIndex = index
+    }
+})
