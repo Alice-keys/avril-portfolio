137734062c757bf1dcd377a874f04d51ab1ab1d3
avril <u20231d637@upc.edu.pe>
2026-03-16 22:35:05 -0500

    feat(layout): add footer component

---
+++ src/shared/presentation/components/footer-content.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+import { Developer } from '../../../portfolio/domain/model/developer.entity.js'
+
+const { t } = useI18n()
+
+defineProps({ developer: { type: Developer, required: true } })
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer-left">
+      <p>© 2026 {{ developer.name }} | {{ developer.title }}</p>
+    </div>
+    <div class="footer-socials">
+      <a v-for="social in developer.socialLinks"
+         :key="social.platform"
+         :href="social.url"
+         target="_blank">
+        <i :class="social.iconUrl"/>
+      </a>
+    </div>
+    <div class="footer-made-with">
+      <p>{{ t('footer.made-with') }}</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+</style>
