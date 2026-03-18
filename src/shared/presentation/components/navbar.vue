b64de1fc92ced211ddeee8555d216ed32d0560fb
avril <u20231d637@upc.edu.pe>
2026-03-17 20:52:27 -0500

    feat(layout): add navbar styles.

--- src/shared/presentation/components/navbar.vue
+++ src/shared/presentation/components/navbar.vue
@@ -18,6 +18,7 @@ function scrollToSection(sectionId) {
 
 <template>
   <nav class="navbar">
+    <div class="navbar-placeholder"></div>
     <div class="navbar-menu desktop">
       <a @click="scrollToSection('home')">{{ t('nav.home') }}</a>
       <a @click="scrollToSection('about')">{{ t('nav.about') }}</a>
@@ -29,17 +30,44 @@ function scrollToSection(sectionId) {
       <button @click="toggleMenu">
         <i class="pi pi-bars"/>
       </button>
-      <div v-if="menuVisible" class="navbar-dropdown">
-        <a @click="scrollToSection('home')">{{ t('nav.home') }}</a>
-        <a @click="scrollToSection('about')">{{ t('nav.about') }}</a>
-        <a @click="scrollToSection('portfolio')">{{ t('nav.portfolio') }}</a>
-        <a @click="scrollToSection('skills')">{{ t('nav.skills') }}</a>
-        <a @click="scrollToSection('contact')">{{ t('nav.contact') }}</a>
-      </div>
     </div>
     <language-switcher/>
   </nav>
 </template>
 
 <style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 60px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #000000;
+  z-index: 100;
+}
+
+.navbar-menu {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.navbar-menu a {
+  color: #ffffff;
+  text-decoration: none;
+  margin-left: 40px;
+  cursor: pointer;
+}
+
+.navbar-mobile {
+  display: none;
+}
+
+.navbar-placeholder {
+  width: 200px;
+}
+
 </style>
